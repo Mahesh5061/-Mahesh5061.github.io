d26926e75c2a70f8670c1c56601b0e430ba26a59
Maheshk0198 <119738289+Mahesh5061@users.noreply.github.com>
2026-02-11 17:54:06 +0530

    base-pages-1

---
+++ my-app/app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <section className="px-10 py-16">
+      <h2 className="text-4xl font-bold mb-6">Contact</h2>
+      <p>Email: maheshkalaga535@gmail.com</p>
+      <p>GitHub: https://github.com/Mahesh5061</p>
+      <p>LinkedIn: www.linkedin.com/in/mahesh-kalaga-830494225</p>
+    </section>
+  );
+}
---
+++ my-app/app/projects/page.tsx
@@ -0,0 +1,12 @@
+export default function Projects() {
+  return (
+    <section className="px-10 py-16">
+      <h2 className="text-4xl font-bold mb-6">Projects</h2>
+      <ul className="space-y-4">
+        <li>Flinn Sceintific</li>
+        <li>URL Scraper Tool</li>
+        <li>Optimizely CMS Integration</li>
+      </ul>
+    </section>
+  );
+}
